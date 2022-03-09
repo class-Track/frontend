8a8af60856f2342ea825434366145569e8ec3571
Ignacio Tampe <Igtampe@gmail.com>
2022-03-09 16:09:01 -0400

    Separate settings and user button

--- src/components/navmenu/UserButton.js
+++ src/components/navmenu/UserButton.js
@@ -1,4 +1,4 @@
-import { Button, Divider, Menu, MenuItem } from "@mui/material";
+import { Button, Divider, IconButton, Menu, MenuItem } from "@mui/material";
 import React, { useState } from "react";
 
 export default function UserButton(props) {
@@ -10,22 +10,11 @@ export default function UserButton(props) {
     const handleClose = () => { setAnchorEl(null); };
   
     return (
-      <div>
-        <Button onClick={handleClick} style={{ textTransform: 'none' }}>
-            <img src={props.User.imageURL === "" ? "/Images/Blankperson.png" : props.User.imageURL} alt="Profile" width="30px" style={{ margin: "5px", marginLeft: "10px" }} />
-        </Button>
-        
-        <Menu anchorEl={anchorEl} open={open} onClose={handleClose} >
-          {/* Are we going to have PFPs? 
-            <MenuItem onClick={handlePickerOpen} disabled={!props.User.isAdmin && !props.User.isUploader}>Change Profile</MenuItem>
-            <Divider />
-          */}
-          <MenuItem onClick={()=>{}}>My Profile</MenuItem>
-          <Divider />
-          <MenuItem onClick={()=>{}}>Change Password</MenuItem>
-          <MenuItem onClick={()=>{}}>Log Out</MenuItem>
-        </Menu>
-      </div>
+        <a href="/profile">
+            <IconButton>
+                <img src={props.User.imageURL === "" ? "/icons/person.png" : props.User.imageURL} alt="Profile" width="30px"/>
+            </IconButton>
+        </a>
     );
   
   }
