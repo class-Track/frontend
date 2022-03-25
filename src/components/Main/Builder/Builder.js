000344975f41d234fae785b4a1b32191adff997a
Francis Maclang <josefrancisco.maclang@upr.edu>
2022-03-24 22:36:57 -0400

    added two separate pages for courses and semesters so that those developing them can avoid merge conflicts with ease

--- src/components/Main/Builder/Builder.js
+++ src/components/Main/Builder/Builder.js
@@ -1,12 +1,30 @@
 import { useHistory } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
+import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import axios from 'axios';
+import Courses from './Courses/Courses';
+import Semesters from './Semesters/Semesters';
 
 export default function Builder() {
 
     return (
-        <p>Builder works!</p>
+        <div>
+            <p>Builder works!</p>
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+            >
+                <Grid item>
+                    <Courses />
+                </Grid>
+                <Grid item>
+                    <Semesters />
+                </Grid>
+            </Grid>
+        </div>
     )
 
 }
---
+++ src/components/Main/Builder/Semesters/Semesters.js
@@ -0,0 +1,5 @@
+export default function Semesters() {
+    return (
+        <p>Semesters works!</p>
+    )
+}
